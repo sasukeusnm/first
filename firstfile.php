fea7a03c7501f860c801d8d054c3e1506ee97709
sasukeusnm <79301812+sasukeusnm@users.noreply.github.com>
2021-04-01 23:52:47 +0900

    Update firstfile.php

--- firstfile.php
+++ firstfile.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<head>
-  <meta ="utf-8">
-  <language="ja">
-</head>
-<body>
-<p><?php echo "myname"?></p>
-</body>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+  
+  </head>
+  <body>
+    <p><?php echo "myname" ?></p>
+  </body>
